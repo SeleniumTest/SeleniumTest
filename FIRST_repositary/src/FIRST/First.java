70f27a1daaee4c2b007af2a7f4fec1d84f5b210d
rokommi <rokommi@DIN35005056.corp.capgemini.com>
2018-10-26 15:42:36 +0530

    added class

--- FIRST_repositary/src/FIRST/First.java
+++ FIRST_repositary/src/FIRST/First.java
@@ -1,9 +1,18 @@
 package FIRST;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class First {
 
-	public static void main(String[] args) {
+	
 		// TODO Auto-generated method stub
+		
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\rokommi\\Documents\\T Mobile\\selenium jar files\\chromedriver_win32\\chromedriver.exe");
+
+			WebDriver driver=new ChromeDriver();
 System.out.println("first");
 	}
 
--- First_project/src/selenium_practice/Practice.java
+++ First_project/src/selenium_practice/Practice.java
@@ -15,11 +15,18 @@ public class Practice {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\rokommi\\Documents\\T Mobile\\selenium jar files\\chromedriver_win32\\chromedriver.exe");
 
 		WebDriver driver=new ChromeDriver();
-		driver.get("https://www.amazon.in/");
-		Actions a=new Actions(driver);
-		a.moveToElement(driver.findElement(By.xpath("//a[@id='nav-link-yourAccount']"))).build().perform();
-		a.moveToElement(driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
-		a.moveToElement(driver.findElement(By.xpath("//a[@id='nav-link-yourAccount']"))).contextClick().build().perform();
+		driver.get("http://www.qaclickacademy.com/practice.php");
+		driver.findElement(By.xpath("//input[@id='alertbtn']")).click();
+		String text=driver.switchTo().alert().getText();
+		System.out.println(text);
+		if(text.equalsIgnoreCase("Hello , share this practice page and share your knowledge")) {
+			System.out.println("pass");
+		}
+		
+		//Actions a=new Actions(driver);
+		//a.moveToElement(driver.findElement(By.xpath("//a[@id='nav-link-yourAccount']"))).build().perform();
+		//a.moveToElement(driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+		//a.moveToElement(driver.findElement(By.xpath("//a[@id='nav-link-yourAccount']"))).contextClick().build().perform();
 	}
 
 }
