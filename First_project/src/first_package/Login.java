45de5bb87981b98aa9735849c46dedc77e751e9c
rokommi <rokommi@DIN39000878.corp.capgemini.com>
2018-11-27 09:17:09 +0530

    cc

--- First_project/src/first_package/Login.java
+++ First_project/src/first_package/Login.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-
+///comment
 public class Login {
 
 	public static void main(String[] args) {
